ae016a4e815c27387ab875f2088d05422dd3d34e
Terroor blade <sonyalest12@gmail.com>
2025-02-27 17:37:08 +0000

    Save progress: 1025.1

--- insert_data.sh
+++ insert_data.sh
@@ -55,8 +55,15 @@ done
 
 cat students_test.csv | while IFS="," read FIRST LAST MAJOR GPA
 do
-  if [[ $FIRST != "first_name" ]]
+  if [[ $FIRST != first_name ]]
   then
+    #get major_id
 
+    #if not found
+
+    #set to null
+
+    #insert student
+    
   fi
 done
